d3e4df0e8b00d14b6c83b9d57543cbee7895e374
groana <contact@groanamelendez.com>
2020-06-23 01:54:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -14,8 +14,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(arr)
-  puts 'display board'
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
