15a6847d2d4ee54779a376eeb1400eca05b6a8cd
groana <contact@groanamelendez.com>
2020-06-23 01:46:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -13,13 +13,13 @@
 
 # # display_board(board)
 
-def display_board(board)
 separator = "|"
 lines = "-----------"
 
-puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
-puts "#{lines}"
-puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
-puts "#{lines}"
-puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
+def display_board(board)
+  puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+  puts "#{lines}"
+  puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+  puts "#{lines}"
+  puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
 end
